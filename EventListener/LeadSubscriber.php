e1bb0ec022fa3f35868e03881e287b24928098ed
lczancanella <31701417+lczancanella@users.noreply.github.com>
2025-08-05 14:06:11 -0300

    Update LeadSubscriber.php

--- EventListener/LeadSubscriber.php
+++ EventListener/LeadSubscriber.php
@@ -2,61 +2,39 @@
 
 namespace MauticPlugin\CpfCnpjValidationBundle\EventListener;
 
+use Mautic\LeadBundle\LeadEvents;
 use Mautic\LeadBundle\Event\LeadEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class LeadSubscriber implements EventSubscriberInterface
 {
-    public static function getSubscribedEvents()
+    public static function getSubscribedEvents(): array
     {
         return [
-            'mautic.lead_pre_save' => ['onLeadPreSave', 0],
+            LeadEvents::LEAD_PRE_SAVE => ['onLeadPreSave', 0],
         ];
     }
 
-    public function onLeadPreSave(LeadEvent $event)
+    public function onLeadPreSave(LeadEvent $event): void
     {
         $lead = $event->getLead();
-
         $cpf = $lead->getFieldValue('cpf');
-        $cnpj = $lead->getFieldValue('cnpj');
 
-        if ($cpf && !$this->isValidCPF($cpf)) {
-            throw new \RuntimeException('CPF inválido.');
-        }
-
-        if ($cnpj && !$this->isValidCNPJ($cnpj)) {
-            throw new \RuntimeException('CNPJ inválido.');
+        if ($cpf && !$this->validaCPF($cpf)) {
+            throw new \Exception("CPF inválido: $cpf");
         }
     }
 
-    private function isValidCPF(string $cpf): bool
+    private function validaCPF(string $cpf): bool
     {
         $cpf = preg_replace('/[^0-9]/', '', $cpf);
         if (strlen($cpf) != 11 || preg_match('/(\d)\1{10}/', $cpf)) return false;
-
         for ($t = 9; $t < 11; $t++) {
             for ($d = 0, $c = 0; $c < $t; $c++) {
                 $d += $cpf[$c] * (($t + 1) - $c);
             }
             $d = ((10 * $d) % 11) % 10;
             if ($cpf[$c] != $d) return false;
-        }
-        return true;
-    }
-
-    private function isValidCNPJ(string $cnpj): bool
-    {
-        $cnpj = preg_replace('/[^0-9]/', '', $cnpj);
-        if (strlen($cnpj) != 14 || preg_match('/(\d)\1{13}/', $cnpj)) return false;
-
-        for ($t = 12; $t < 14; $t++) {
-            for ($d = 0, $p = 0, $c = $t - 7; $p < $t; $p++) {
-                $d += $cnpj[$p] * $c--;
-                if ($c < 2) $c = 9;
-            }
-            $d = ((10 * $d) % 11) % 10;
-            if ($cnpj[$p] != $d) return false;
         }
         return true;
     }
