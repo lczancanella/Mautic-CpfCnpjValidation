599fbbd4559c6e25acba95449483d81005478baa
lczancanella <31701417+lczancanella@users.noreply.github.com>
2025-08-05 16:36:12 -0300

    Update config.php

--- Config/config.php
+++ Config/config.php
@@ -2,8 +2,8 @@
 
 return [
     'name'        => 'CpfCnpjValidation',
-    'description' => 'Valida campos de CPF (contatos) e CNPJ (empresas). v1.0.2',
-    'version'     => '1.0.2',
+    'description' => 'Valida campos de CPF (contatos) e CNPJ (empresas). v1.1',
+    'version'     => '1.1',
     'author'      => 'Luiz Carlos Zancanella Junior',
     'services'    => [
         'events' => [
