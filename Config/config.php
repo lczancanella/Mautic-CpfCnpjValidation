dd3fdef238d4aa77261deb0264495331ab2f6ed3
lczancanella <31701417+lczancanella@users.noreply.github.com>
2025-08-05 16:27:11 -0300

    Update config.php

--- Config/config.php
+++ Config/config.php
@@ -2,18 +2,18 @@
 
 return [
     'name'        => 'CpfCnpjValidation',
-    'description' => 'Valida campos de CPF (contatos) e CNPJ (empresas). v1.0.1',
-    'version'     => '1.0.1',
+    'description' => 'Valida campos de CPF (contatos) e CNPJ (empresas). v1.0.2',
+    'version'     => '1.0.2',
     'author'      => 'Luiz Carlos Zancanella Junior',
     'services'    => [
         'events' => [
             'cpfcnpjvalidation.lead.subscriber' => [
                 'class' => \MauticPlugin\CpfCnpjValidationBundle\EventListener\LeadSubscriber::class,
-                'tags'  => [['name' => 'kernel.event_subscriber']],
+                'tags'  => ['kernel.event_subscriber'],
             ],
             'cpfcnpjvalidation.company.subscriber' => [
                 'class' => \MauticPlugin\CpfCnpjValidationBundle\EventListener\CompanySubscriber::class,
-                'tags'  => [['name' => 'kernel.event_subscriber']],
+                'tags'  => ['kernel.event_subscriber'],
             ],
         ],
     ],
