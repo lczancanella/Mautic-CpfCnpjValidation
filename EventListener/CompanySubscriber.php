d63c95adcc1e190478a254ff0a49f965f215f862
lczancanella <31701417+lczancanella@users.noreply.github.com>
2025-08-05 15:10:21 -0300

    Update CompanySubscriber.php

--- EventListener/CompanySubscriber.php
+++ EventListener/CompanySubscriber.php
@@ -2,46 +2,48 @@
 
 namespace MauticPlugin\CpfCnpjValidationBundle\EventListener;
 
-use Mautic\FormBundle\Event\FormEvents;
-use Mautic\FormBundle\Event\ValidationEvent;
+use Mautic\LeadBundle\Event\LeadEvent;
+use Mautic\LeadBundle\LeadEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
-class CompanySubscriber implements EventSubscriberInterface
+class LeadSubscriber implements EventSubscriberInterface
 {
     public static function getSubscribedEvents(): array
     {
         return [
-            FormEvents::VALIDATE_FORM => ['onValidate', 0],
+            LeadEvents::LEAD_PRE_SAVE => ['onLeadPreSave', 0],
         ];
     }
 
-    public function onValidate(ValidationEvent $event): void
+    public function onLeadPreSave(LeadEvent $event): void
     {
-        $data = $event->getData();
+        $lead = $event->getLead();
 
-        // Verifica se existe o campo personalizado "cnpj"
-        if (isset($data['cnpj']) && !$this->isValidCnpj($data['cnpj'])) {
-            $event->addError('cnpj', 'O CNPJ informado é inválido.');
+        if ($lead->hasField('cpf')) {
+            $cpf = $lead->getFieldValue('cpf');
+            if (!empty($cpf) && !$this->isValidCpf($cpf)) {
+                throw new \InvalidArgumentException('O CPF informado é inválido.');
+            }
         }
     }
 
-    private function isValidCnpj(string $cnpj): bool
+    private function isValidCpf(string $cpf): bool
     {
-        $cnpj = preg_replace('/[^0-9]/', '', $cnpj);
+        $cpf = preg_replace('/[^0-9]/', '', $cpf);
 
-        if (strlen($cnpj) != 14 || preg_match('/(\d)\1{13}/', $cnpj)) {
+        if (strlen($cpf) !== 11 || preg_match('/(\d)\1{10}/', $cpf)) {
             return false;
         }
 
-        for ($t = 12; $t < 14; $t++) {
+        for ($t = 9; $t < 11; $t++) {
             $d = 0;
-            $c = 0;
-            for ($m = $t - 7, $i = 0; $i < $t; $i++) {
-                $d += $cnpj[$i] * $m--;
-                if ($m < 2) $m = 9;
+            for ($c = 0; $c < $t; $c++) {
+                $d += $cpf[$c] * (($t + 1) - $c);
             }
             $d = ((10 * $d) % 11) % 10;
-            if ($cnpj[$t] != $d) return false;
+            if ($cpf[$c] != $d) {
+                return false;
+            }
         }
 
         return true;
